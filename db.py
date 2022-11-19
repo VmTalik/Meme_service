b608b5aa3160ca008b74d88904f4500641116ef0
VmTalik <vmarlyan@mail.ru>
2022-11-19 19:25:35 +0300

    Add code

--- db.py
+++ db.py
@@ -1,6 +1,12 @@
 import sqlalchemy
 import databases
+import ormar
 
 metadata = sqlalchemy.MetaData()
 database = databases.Database("sqlite:///sqlite_meme.db")
 engine = sqlalchemy.create_engine("sqlite:///sqlite_meme.db")
+
+
+class MainMeta(ormar.ModelMeta):
+    metadata = metadata
+    database = database
